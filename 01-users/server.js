677c84bf528c2f61af89213d6d9d9733a70054d6
DiogoAMFerreira <DiogoAMFerreira@users.noreply.github.com>
2025-04-11 13:03:10 +0100

    Finished basic setup

--- 01-users/server.js
+++ 01-users/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
 app.use(
   "/graphql",
   graphqlHTTP({
+    schema,
     graphiql: true, //Development tool that allows us to make queries against our development server
   })
 );
