9235f91806d295598ceaeae070b4c5801fa9895a
DiogoAMFerreira <DiogoAMFerreira@users.noreply.github.com>
2025-08-29 17:27:42 +0100

    Rendering details page

--- 03-react-client-lyrics/client/components/songs/SongDetails.js
+++ 03-react-client-lyrics/client/components/songs/SongDetails.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import getSong from '../../queries/getSong'
 
 class SongDetail extends Component {
+    renderLoading() {
+        return <div>Song Loading...</div>
+    }
+    renderNotFound() {
+        return <div>Song not found...</div>
+    }
     render() {
+        if (this.props.data.loading === true) {
+            return this.renderLoading()
+        }
+
+        const { song } = this.props.data
+        if (!song) {
+            return this.renderNotFound()
+        }
+
         return (
             <div>
-                <h3>Song Details</h3>
+                <h3>{song.title}</h3>
             </div>
         )
     }
 }
 
-export default SongDetail
+export default graphql(getSong, {
+    options: (props) => {
+        return { variables: { id: props.params.id } }
+    },
+})(SongDetail)
