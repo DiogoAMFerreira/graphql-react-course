56d35c52d395838b3e769cd7bb90827a1b48f6c6
DiogoAMFerreira <DiogoAMFerreira@users.noreply.github.com>
2025-08-29 15:50:03 +0100

    Start of song detail page

--- 03-react-client-lyrics/client/index.js
+++ 03-react-client-lyrics/client/index.js
@@ -7,6 +7,7 @@ import { ApolloProvider } from 'react-apollo'
 import App from './components/App'
 import SongList from './components/songs/SongList'
 import SongCreate from './components/songs/SongCreate'
+import SongDetail from './components/songs/SongDetails'
 
 const client = new ApolloClient({})
 
@@ -17,7 +18,8 @@ const Root = () => {
                 <Route path="/" component={App}>
                     <IndexRoute component={SongList}></IndexRoute>
                 </Route>
-                <Route path="songs/new" component={SongCreate}></Route>
+                <Route path="songs/new" component={SongCreate} />
+                <Route path="songs/:id" component={SongDetail} />
             </Router>
         </ApolloProvider>
     )
