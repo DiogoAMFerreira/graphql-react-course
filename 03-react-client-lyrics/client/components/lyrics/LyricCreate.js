c2ef7e2bfad07670dcf14d8e31075b85176c7e49
DiogoAMFerreira <DiogoAMFerreira@users.noreply.github.com>
2025-09-22 17:32:24 +0100

    64 - Add lyric to song done

--- 03-react-client-lyrics/client/components/lyrics/LyricCreate.js
+++ 03-react-client-lyrics/client/components/lyrics/LyricCreate.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
-
+import { graphql } from 'react-apollo'
+import addLyricToSong from '../../mutations/addLyricToSong'
 class LyricCreate extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { content: '' }
+    }
+
+    onSubmit(event) {
+        event.preventDefault()
+
+        this.props.mutate({
+            variables: {
+                songId: this.props.songId,
+                content: this.state.content,
+            },
+            // refetchQueries: [{
+        })
+        // Clear the input
+        this.setState({ content: '' })
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.onSubmit.bind(this)}>
                 <label>Add a Lyric</label>
-                <input />
+                <input
+                    onChange={(event) =>
+                        this.setState({ content: event.target.value })
+                    }
+                    value={this.state.content}
+                />
             </form>
         )
     }
 }
 
-export default LyricCreate
+export default graphql(addLyricToSong)(LyricCreate)
