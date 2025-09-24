660cd24c1315d86065ae94d5b3eb64fd76b47aae
DiogoAMFerreira <DiogoAMFerreira@users.noreply.github.com>
2025-09-24 17:56:18 +0100

    69 nice - like button start

--- 03-react-client-lyrics/client/components/lyrics/LyricList.js
+++ 03-react-client-lyrics/client/components/lyrics/LyricList.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
 
 class LyricList extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
+    onLike(id) {}
 
     renderLyrics() {
         return this.props.lyrics.map(({ id, content, likes }) => {
             return (
                 <li className="collection-item" key={id}>
                     {content}
+                    <div>
+                        {likes}
+                        <i
+                            onClick={() => this.onLike(id)}
+                            className="material-icons right"
+                        >
+                            thumb_up
+                        </i>
+                    </div>
                 </li>
             )
         })
