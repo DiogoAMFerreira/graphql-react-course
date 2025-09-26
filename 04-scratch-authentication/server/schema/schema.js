d4f8ec4d34c53653afa0ae77acd0c90036b269c4
DiogoAMFerreira <DiogoAMFerreira@users.noreply.github.com>
2025-09-26 15:31:19 +0100

    84 - SignUp Mutation

--- 04-scratch-authentication/server/schema/schema.js
+++ 04-scratch-authentication/server/schema/schema.js
@@ -2,6 +2,7 @@ const graphql = require('graphql')
 const { GraphQLSchema } = graphql
 
 const RootQueryType = require('./types/RootQueryType')
+const mutation = require('./mutations')
 
 module.exports = new GraphQLSchema({
     query: RootQueryType,
