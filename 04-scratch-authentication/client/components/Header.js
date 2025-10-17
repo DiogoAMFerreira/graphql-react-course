c8f8bc54a04da8efa6587dea7b68ab7b6a9956a9
DiogoAMFerreira <DiogoAMFerreira@users.noreply.github.com>
2025-10-17 15:20:31 +0100

    Start of Landing page

---
+++ 04-scratch-authentication/client/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+/**
+ * Header component
+ *
+ * Class based component since we will add state and lifecycle methods later
+ */
+class Header extends Component {
+    render() {
+        return <div>Header</div>
+    }
+}
+
+export default Header
