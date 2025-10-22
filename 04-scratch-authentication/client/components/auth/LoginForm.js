0cf1d7ecfbd311c1d035047c7c3766b82e1be78f
DiogoAMFerreira <DiogoAMFerreira@users.noreply.github.com>
2025-10-22 13:10:50 +0100

    102 - Error handling

--- 04-scratch-authentication/client/components/auth/LoginForm.js
+++ 04-scratch-authentication/client/components/auth/LoginForm.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react'
 import AuthForm from './AuthForm'
 import { graphql } from 'react-apollo'
 import LoginMutation from '../../mutations/Login'
+import CurrentUserQuery from '../../queries/CurrentUser'
 
 class LoginForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            errors: [],
+        }
+    }
+
     onSubmit({ email, password }) {
-        this.props.mutate({
-            variables: { email, password },
-        })
+        this.props
+            .mutate({
+                variables: { email, password },
+                refetchQueries: [{ query: CurrentUserQuery }],
+            })
+            .catch((err) => {
+                const errorMessages = err.graphQLErrors.map(
+                    ({ message }) => message
+                )
+
+                this.setState({ errors: errorMessages })
+            })
     }
 
     render() {
